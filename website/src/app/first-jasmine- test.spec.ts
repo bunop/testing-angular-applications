a99d908017ae425d68921296a6ea0f790dd189a4
Paolo Cozzi <bunop@libero.it>
2022-12-22 18:29:03 +0100

    :sparkles: create first tests

---
+++ website/src/app/first-jasmine- test.spec.ts
@@ -0,0 +1,10 @@
+// example test using jasmine
+describe('Chapter 2 tests', () => {
+  it('Our first Jasmine test', () => {
+    expect(true).toBe(true);
+  });
+
+  it('Expect 2+2=4', () => {
+    expect(2+2).toBe(4);
+  })
+});
